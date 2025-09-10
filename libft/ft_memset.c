67930f7aaf442502f4445d8f7d7a7cbee282f941
Maria <166420887+mariapuyo70@users.noreply.github.com>
2025-09-10 11:10:30 +0200

    Subida ft_memset

---
+++ libft/ft_memset.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpuyo-ro <mpuyo-ro@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/10 11:07:37 by mpuyo-ro          #+#    #+#             */
+/*   Updated: 2025/09/10 11:07:37 by mpuyo-ro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "stddef.h"
+
+void    *ft_memset(void *b, int c, size_t len)
+{
+    size_t  i;
+
+    i = 0;
+    while (i < len)
+    {
+        ((unsigned char *) b)[i] = (unsigned char) c;
+        i++;
+    }
+    return (b);
+}
